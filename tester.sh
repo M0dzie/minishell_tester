e9f8864f2956b88f67a1e5c678aa1308ff758ad0
Modzie <110020963+M0dzie@users.noreply.github.com>
2023-03-06 13:09:56 +0100

    fix test norm and makefile

--- tester.sh
+++ tester.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 green='\033[0;32m'
+blue='\033[1;34m'
 red='\033[0;31m'
 yellow='\033[0;33m'
 bold="\033[1m"
@@ -19,26 +20,29 @@ echo
 
 exec="minishell $>"
 
+## Make project ##
 if test -f Makefile; then
     make
+	echo 
+	if [[ $? -eq 0 ]]; then
+		echo -e "${green}${bold}Compilation succeeded ✓${nc}"
+	else
+		echo -e "${red}${bold}⚠️ Compilation failed ⚠️${nc}"
+		exit 1
+	fi
+	echo 
 fi
 
-norminette_success=true
-find . -name "*.c" -exec sh -c '
-    if test -f norminette; then
-        ./norminette -R CheckForbiddenSourceHeader "$1"
-        if [ $? -ne 0 ]; then
-            exit 1
-        fi  
-    fi
-' sh {} \; || norminette_success=false
-
-if [ $norminette_success = true ]; then
-    echo -e "${green}${bold}Norm OK${nc}"
+## Norm test ##
+printf "${blue}${bold}#####		TEST NORM		#####\n\n${nc}"
+norminette -R CheckForbiddenSourceHeader 1>/dev/null
+if [[ $? -eq 0 ]]; then
+    echo -e "${green}${bold}Norm OK ✓${nc}"
 else
-    echo -e "${red}${bold}Norm KO${nc}"
+    echo -e "${red}${bold}⚠️ Norm KO ⚠️${nc}"
 fi
 
+## Test functions ##
 exec_cmd()
 {
     cmd="$1"
@@ -51,9 +55,9 @@ check_result()
     expected_output="$1"
     your_output="$2"
     if [ "$expected_output" = "$your_output" ]; then
-        echo -e "${green}${bold}Success${nc}"
+        echo -e "${green}${bold}Success ✓${nc}"
     else
-        echo -e "${red}${bold}Fail${nc}"
+        echo -e "${red}${bold}⚠️ Fail ⚠️${nc}"
         echo "Expected output : $expected_output"
         echo  "Your ouput : $your_output"
     fi
